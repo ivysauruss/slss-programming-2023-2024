0af0f843709ad9947ffc5e99030aff3f5979cebc
Alissa Xu <alissa01288@gmail.com>
2023-11-23 11:12:56 -0800

    Added to cookies

--- cookies.py
+++ cookies.py
@@ -3,36 +3,45 @@
 # 21 November 2023
 
 import turtle
-
 # Make baker turtle
 baker_turtle = turtle.Turtle()
 baker_turtle.color("brown")
 
-# Draw outline of cookie
-baker_turtle.penup()
-baker_turtle.goto(-5, -30)
-baker_turtle.pendown()
-baker_turtle.circle(30)
-baker_turtle.penup()
 
-# Add five chips
-baker_turtle.color("black")
+def make_cookie(x: int, y: int):
+    """Draws a cookie on the screen at (x, y)
+    
+    Params: 
+    x - x-coordinate of the centre of cookie
+    y - y-coordinate of the centre of cookie
+    """
+    # Set up the outline of the cookie
+    baker_turtle.color("brown")
+    baker_turtle.penup()
+    baker_turtle.goto(-5 + x, -30 + y)
+    baker_turtle.circle(30)
+    baker_turtle.penup()
 
-baker_turtle.goto(0,0)
-baker_turtle.stamp()
+    # Add five chips
+    baker_turtle.color("black")
 
-# Top-right chip, bottom-right, bottom-left, top left
-baker_turtle.goto(10,10)
-baker_turtle.stamp()
+    baker_turtle.goto( + x,0 + y)
+    baker_turtle.stamp()
 
-baker_turtle.goto(10,-10)
-baker_turtle.stamp()
+    # Top-right chip, bottom-right, bottom-left, top left
+    baker_turtle.goto(10 + x, 10 + y)
+    baker_turtle.stamp()
 
-baker_turtle.goto(-10,-10)
-baker_turtle.stamp()
+    baker_turtle.goto(10 + x, -10 + y)
+    baker_turtle.stamp()
 
-baker_turtle.goto(-10,10)
-baker_turtle.stamp()
+    baker_turtle.goto(-10 + x, -10 + y)
+    baker_turtle.stamp()
 
+    baker_turtle.goto(-10 + x ,10 + y)
+    baker_turtle.stamp()
+
+
+make_cookie(100,100)
 
 turtle.done()
