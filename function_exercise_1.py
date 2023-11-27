ae4f96925d61a428ffc31dd2e138228b82bb4042
Alissa Xu <alissa01288@gmail.com>
2023-11-27 11:03:33 -0800

    Added to functions

---
+++ function_exercise_1.py
@@ -0,0 +1,19 @@
+# Function Exercise 1
+# Author: Alissa Xu
+# Date created: 27 November 2023
+
+def stars(number: int) -> None:
+    """Creates stars based on the argument
+    Results in integer.
+
+    Params: 
+    
+    number - number of stars created"""
+
+    starnumber = number
+
+    for i in range(number):
+        print("*")
+
+stars(3)
+
