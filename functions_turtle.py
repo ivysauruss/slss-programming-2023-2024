069ab1c68b35d402dd33c6404b49b078743cac25
Alissa Xu <alissa01288@gmail.com>
2023-12-04 10:38:09 -0800

    Added functions turtles file

---
+++ functions_turtle.py
@@ -0,0 +1,54 @@
+# Functions and Turtle
+# Author: Alissa Xu
+# 28 November 2023
+
+import turtle 
+
+alfred_river_wilson = turtle.Turtle()
+
+alfred_river_wilson.color("lightgreen")
+alfred_river_wilson.shape("turtle")
+
+def squared(num: float) -> float:
+    """Returns the given number squared"""
+    return ** 2
+
+def draw_square(sidelength: float) -> float: 
+    for _ in range(4):
+        hello.forward(sidelength)
+        hello.left(90)
+
+def draw_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree with initial given length
+    
+    Params:
+    
+    level - number representing levels of branches
+    height - height of the main trucnk in pixels
+    """
+
+    if level > 0:
+        # 1. Draw the branch
+        alfred_river_wilson.forward(height)
+
+        # 2. Turn to the left 
+        alfred_river_wilson.left(39)
+
+        # 3. Draw a smaller tree (current level - 1)
+        draw_tree(level-1, height/1.5)
+
+        # 4. Turn to the right
+        alfred_river_wilson.right(39*2)
+
+        # 5. Draw a smaller tree (current level - 1)
+        draw_tree(level-1, height/1.5)
+
+        # 6. Move back to beginning 
+        alfred_river_wilson.left()
+        alfred_river_wilson.back(height)
+    else: 
+        # Create a level 0 tree, which is a leaf
+        original_colour = alfred_river_wilson.color()
+        alfred_river_wilson.color("green")
+        alfred_river_wilson.stamp()
+        alfred_river_wilson.color(original_colour)
