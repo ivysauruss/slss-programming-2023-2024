7a768fa8232555b47708324e5361e7b1895cf47a
Alissa Xu <alissa01288@gmail.com>
2024-01-09 12:40:14 -0800

    Added

--- colour_helper.py
+++ colour_helper.py
@@ -30,3 +30,27 @@ def pixel_to_grayscale(pixel: tuple) -> tuple:
     gray = int(red * 0.3 + green * 0.59 + blue * 0.11)
 
     return (gray, gray, gray)
+
+def pixel_to_name(pixel: tuple) -> str:
+    """Given a 3-tuple, return a string representing
+    its colour
+
+    Params:
+        pixel = 3-tuple of values (red, green, blue)
+
+    Returns:
+        name of the colour
+    """
+    red, green, blue = pixel
+
+    if red < 200 and blue < 200 and green > 220:
+        return "green"
+    elif red > 170 and green < 60 and blue < 60:
+        return "red"
+    else:
+        return "colour unknown"
+    
+    print(pixel_to_name())
+
+
+    
