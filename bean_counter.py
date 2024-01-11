a1e5db172814b20dd5251d9f9223bf652279177a
Alissa Xu <alissa01288@gmail.com>
2024-01-11 12:09:19 -0800

    Added

--- bean_counter.py
+++ bean_counter.py
@@ -1,37 +1,66 @@
+# Jelly Bean Colour Counter
+# Author: Ubial
+# 9 January 2024
 
+# Version 0.1
+#  - Count red pixels/beans
+#  - Report on the percentage of all red beans
+# Version 0.2
+#  - Count green pixels/beans
+#  - Report on the %age of all green beans
 
+from PIL import Image
 
+import colour_helper
 
+RED_PIXEL = (160, 0, 0)
+GREEN_PIXEL = (0, 160, 0)
 
-
-
-
-from PIL import Image 
-
-import colour_helper 
-
-# Visit every pixel in the image
 jelly_bean_img = Image.open("./Images/Jelly Beans.jpg")
 
 red_pixels = []
+green_pixels = []
 
 # Visit every pixel in the image
-for y in range(jelly_bean_img.height): 
-    for x in range(jelly_bean_img.width): 
+for y in range(jelly_bean_img.height):
+    for x in range(jelly_bean_img.width):
         # Get the pixel information
         current_pixel = jelly_bean_img.getpixel((x, y))
 
-        # If this pixel is red
-        if colour_helper.pixel_to_name(current_pixel) == 'red': 
-            # Store its location somewhere
+        # Detect the current pixel's colour
+        if colour_helper.pixel_to_name(current_pixel) == "red":
             red_pixels.append((x, y))
+        elif colour_helper.pixel_to_name(current_pixel) == "jelly bean green":
+            green_pixels.append((x, y))
 
-print(red_pixels)
+# Create a map of all red pixels "found"
+# Create a new image that stores the map
+#   - the new image should be the same size as original
+orig_image_width = jelly_bean_img.width
+orig_image_height = jelly_bean_img.height
 
-# Count all locations of red pixels 
+green_pixel_map = Image.new("RGB", (orig_image_width, orig_image_height))
+
+# For every pixel location in the red_pixels list
+# Place a red pixel at that location
+for pixel_loc in green_pixels:
+    green_pixel_map.putpixel(pixel_loc, GREEN_PIXEL)
+
+# Save the image
+green_pixel_map.save("./Images/green_pixel_map.jpg")
+green_pixel_map.close()
+
+# Count all the locations of red pixels
 red_pixel_count = len(red_pixels)
+green_pixel_count = len(green_pixels)
 total_pixels = jelly_bean_img.width * jelly_bean_img.height
 
 # Divide by the total pixels in the image
 red_pixel_percentage = red_pixel_count / total_pixels * 100
-print(f"{red_pixel_percentage}%")
+green_pixel_percentage = green_pixel_count / total_pixels * 100
+
+# Generate the report
+print(f"Red Jelly Beans: {round(red_pixel_percentage, 2)}%")
+print(f"Green Jelly Beans: {round(green_pixel_percentage, 2)}%")
+
+jelly_bean_img.close()
--- colour_helper.py
+++ colour_helper.py
@@ -2,6 +2,33 @@
 # Functions that help wither colours
 
 
+def pixel_to_name(pixel: tuple) -> str:
+    """Given a 3-tuple, return a string representing
+    its colour
+
+    Params:
+        pixel = 3-tuple of values (red, green, blue)
+
+    Returns:
+        name of the colour
+    """
+    red, green, blue = pixel
+
+    # TODO: detect jelly bean green
+    if red < 200 and blue < 200 and green > 220:
+        return "green"
+    elif red > 170 and green < 60 and blue < 60:
+        return "red"
+    elif red < 60 and green > 80 and blue < 60:
+        return "jelly bean green"
+    else:
+        return "colour unknown"
+
+
+print(pixel_to_name((180, 3, 2)))
+print(pixel_to_name((255, 255, 255)))
+
+
 def is_light(pixel: tuple) -> bool:
     """Returns True if the pixel is a "light" pixel
 
@@ -23,6 +50,7 @@ def is_light(pixel: tuple) -> bool:
     else:
         return False
 
+
 def pixel_to_grayscale(pixel: tuple) -> tuple:
     """Return a gray version of the given pixel"""
     red, green, blue = pixel
@@ -31,28 +59,20 @@ def pixel_to_grayscale(pixel: tuple) -> tuple:
 
     return (gray, gray, gray)
 
-def pixel_to_name(pixel: tuple) -> str:
-    """Given a 3-tuple, return a string representing
-    its colour
 
-    Params:
-        pixel = 3-tuple of values (red, green, blue)
-
-    Returns:
-        name of the colour
-    """
+def pixel_to_random_effect(pixel: tuple) -> tuple:
+    """Return a random pixel"""
     red, green, blue = pixel
 
-    if red < 200 and blue < 200 and green > 220:
-        return "green"
-    elif red > 170 and green < 60 and blue < 60:
-        return "red"
-    else:
-        return "colour unknown"
-    
-    print(pixel_to_name())
+    red += 30
+    green += 50
+    blue -= 10
 
-##
+    if red > 255:
+        red = 255
+    if green > 255:
+        green = 255
+    if blue < 0:
+        blue = 0
 
-
-    
+    return (red, green, blue)
