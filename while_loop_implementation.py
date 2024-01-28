1769581d3ac4ad2035a8225a9d217a57a3ffdbc4
ivysauruss <144833914+ivysauruss@users.noreply.github.com>
2024-01-27 23:50:44 -0800

    Added

---
+++ while_loop_implementation.py
@@ -0,0 +1,15 @@
+# While Loop Implementation 
+# Alissa Xu 
+# January 27 2024
+
+number = 3 
+int = 0
+
+while number < 200:
+    print("this number fulfills requirements.")
+    number = number + 1 
+    int = int + 1
+    if number == 20: 
+        break
+
+## Validates that 
