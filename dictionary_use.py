ab37782584b6d8b1c400b97d2a93641f0c2ba83d
ivysauruss <144833914+ivysauruss@users.noreply.github.com>
2024-01-28 00:03:45 -0800

    Added

---
+++ dictionary_use.py
@@ -0,0 +1,12 @@
+# Dictionary Use
+# Author: Alissa Xu
+# January 27 2024
+
+information =
+{
+    'name' : 'Alissa',
+    'grade' : '11', 
+    'age' : '17 (as of today!!!!)'
+}
+
+print(information['age'])
